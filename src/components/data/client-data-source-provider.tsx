264fd195d1d2e8827d027ecb1134a8e2816b268a
Denis Korolko <dk@elearningforce.com>
2018-12-13 15:51:47 +0300

    test

--- src/components/data/client-data-source-provider.tsx
+++ src/components/data/client-data-source-provider.tsx
@@ -3,9 +3,11 @@ import { ClientDataSource, ClientDataSourceProps } from '../../infrastructure/da
 
 export class ClientDataSourceProvider extends DataSourceProvider<ClientDataSourceProps> {
     public componentWillReceiveProps(nextProps: ClientDataSourceProps) {
-        if ((this.props.data != nextProps.data)
-            && ((this.props.data == null) || Array.isArray(this.props.data))
-            && ((nextProps.data == null) || Array.isArray(nextProps.data))) {
+        if (
+            this.props.data != nextProps.data &&
+            (this.props.data == null || Array.isArray(this.props.data)) &&
+            (nextProps.data == null || Array.isArray(nextProps.data))
+        ) {
             (this.dataSource as ClientDataSource).dataBind(nextProps.data as any[]);
         }
     }
