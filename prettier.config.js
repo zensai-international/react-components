fce40416444935303acc7e3c4e938e724fac59ec
Denis Korolko <dk@elearningforce.com>
2018-12-14 13:37:07 +0300

    Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    printWidth: 150,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: "avoid",
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: "preserve",
+    htmlWhitespaceSensitivity: "css",
+    endOfLine: "auto"
+  };
